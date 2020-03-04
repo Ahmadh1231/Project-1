04f519e697d1205f5d509f2095b5493bb3c30ed0
Ahmadh1231 <40180434+Ahmadh1231@users.noreply.github.com>
2020-03-03 22:48:55 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,110 +1,117 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 main(){
-cout << "Tic Tac Toe bye Ahmadh Hassan and Patrick charles";
-cout << "the board it lined 1-9, please choose 1, you are X";
-cout << "     #     #     ";
-cout << "  1  #  2  #  3  ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "  4  #  5  #  6  ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "  7  #  8  #  9  ";
+int first_input;
+int max_value1 = 9;
+int least_value1 =2;
+cout << "Tic Tac Toe bye Ahmadh Hassan and Patrick charles\n";
+cout << "the board it lined 1-9, please choose 1, you are X\n";
+cout << "     #     #     \n";
+cout << "  1  #  2  #  3  \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "  4  #  5  #  6  \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "  7  #  8  #  9  \n";
 
-cout << "please choose one";
+cout << "please choose one\n";
 cin >> first_input;
 if (first_input == 1){
-cout << "     #     #     ";
-cout << "  X  #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "  X  #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+
 }
 else if (first_input == 2){
-cout << "     #     #     ";
-cout << "     #  x  #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "     #  x  #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
 }
 else if (first_input == 3){
-cout << "     #     #     ";
-cout << "     #     #  x  ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "     #     #  x  \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
 }
 else if (first_input == 4){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "  x  #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "  x  #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
 }
 else if (first_input == 5){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #  x  #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #  x  #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
 }
 else if (first_input == 6){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #  x  ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #  x  \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
 }
 else if (first_input == 7){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "  x  #     #     ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "  x  #     #     \n";
 }
 else if (first_input == 8){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #  x  #     ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #  x  #     \n";
 }
 else (first_input == 9){
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #     ";
-cout << "#################";
-cout << "     #     #     ";
-cout << "     #     #  x  ";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #     \n";
+cout << "#################\n";
+cout << "     #     #     \n";
+cout << "     #     #  x  \n";
+int iRand = rand() % 9;
 }
-};
 
+};
 
