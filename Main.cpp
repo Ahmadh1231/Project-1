e18ce9c33dcd9abd74d6719c418b0b719e325840
Ahmadh1231 <40180434+Ahmadh1231@users.noreply.github.com>
2020-03-03 22:17:30 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout << "Tic Tac Toe bye Ahmadh Hassan and Patrick charles";
+cout << "the board it lined 1-9, please choose 1, you are X";
+cout << "     #     #     ";
+cout << "  1  #  2  #  3  ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "  4  #  5  #  6  ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "  7  #  8  #  9  ";
+
+cout << "please choose one";
+cin >> first_input;
+if (first_input == 1){
+cout << "     #     #     ";
+cout << "  X  #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 2){
+cout << "     #     #     ";
+cout << "     #  x  #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 3){
+cout << "     #     #     ";
+cout << "     #     #  x  ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 4){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "  x  #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 5){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #  x  #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 6){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #  x  ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+}
+else if (first_input == 7){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "  x  #     #     ";
+}
+else if (first_input == 8){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #  x  #     ";
+}
+else (first_input == 9){
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #     ";
+cout << "#################";
+cout << "     #     #     ";
+cout << "     #     #  x  ";
+}
+};
+
+
